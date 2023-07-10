fafc62c30ced94a560f9abcd47a92df7f4bf47e4
sueunal <doms@kakao.com>
2023-07-11 01:23:57 +0900

    Initial Commit

---
+++ ShouldEat/ShouldEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShouldEatApp.swift
+//  ShouldEat
+//
+//  Created by sueun kim on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct ShouldEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
