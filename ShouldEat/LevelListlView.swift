55c33a0e89e3934084038e70b64e45062802fc54
sueunal <36671600+sueunal@users.noreply.github.com>
2023-08-09 17:13:35 +0900

    [➕add] : SwiftSoup으로 크롤링 crew()함수작성
    이미지 크롤링하여 AsyncImage로 화면에 보여지도록 작성

---
+++ ShouldEat/LevelListlView.swift
@@ -0,0 +1,20 @@
+//
+//  LevelListlView.swift
+//  ShouldEat
+//
+//  Created by sueun kim on 2023/08/07.
+//
+
+import SwiftUI
+
+struct LevelListlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LevelListlView_Previews: PreviewProvider {
+    static var previews: some View {
+        LevelListlView()
+    }
+}
